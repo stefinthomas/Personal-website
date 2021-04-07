a4f9ce4ccfff77562bfa3e19807b1931ad73bea6
stefinthomas <79510574+stefinthomas@users.noreply.github.com>
2021-04-07 14:33:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -70,3 +70,21 @@ $(document).ready(function(){
         }
     });
 });
+
+$(".contact-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbyuULyzbcwe2dbURi9zmlTjNpRuS0pSW8JvTdMxsabQeTY4nGZuO8TVyTMjqzhc3c9B/exec",
+        data:$(".contact-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+    })
+})
