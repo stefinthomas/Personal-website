d3d63700a7ae52078d4996d9f0c4197f1ae0a5ae
stefinthomas <79510574+stefinthomas@users.noreply.github.com>
2023-10-15 12:05:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Web Designer", "UI/UX Designer" ],
+        strings: ["Business Analyst", "Data Analyst", "Web Designer", "UI/UX Designer" ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Web Designer", "UI/UX Designer" ],
+        strings: ["Business Analyst", "Data Analyst", "Web Designer", "UI/UX Designer" ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
